74abd740ad83190615aae533c3a81c54509d77bb
LaankyTTV <j4ck8610@gmail.com>
2025-02-10 06:53:34 +1300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ClashRoyaleAI",
+    version="1.0",
+    install_requires=[
+        "tensorflow",
+        "keras",
+        "opencv-python",
+        "pyautogui",
+        "numpy",
+        "matplotlib",
+        "h5py",
+        "pillow"
+    ],
+)
